4c03885f25c4855fe0bb7bf2f46cfe63d1f5c3d7
parshya <prashsingh1997@gmail.com>
2024-06-10 12:41:05 +0530

    Added functionality to add new contact

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,12 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddressBook {
     private List<Contact> contacts;
+    Scanner scanner = new Scanner(System.in);
 
     public AddressBook() {
         this.contacts = new ArrayList<>();
     }
 
+    public void addContact() {
+
+
+        System.out.println("Enter contact details:");
+        System.out.print("First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Last Name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+        System.out.print("City: ");
+        String city = scanner.nextLine();
+        System.out.print("State: ");
+        String state = scanner.nextLine();
+        System.out.print("Zip: ");
+        String zip = scanner.nextLine();
+        System.out.print("Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+        contacts.add(contact);
+    }
+
+    public void showContacts() {
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
 
 }
--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        AddressBook addressBook=new AddressBook();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Choose the option : ");
+        int option;
+        String op = "";
+        do {
+            System.out.println("Press 1 for adding contact details: ");
+            System.out.println("Press 0 for display contact details: ");
+            option=scanner.nextInt();
+            if(option==1)
+            {
+               addressBook.addContact();
+            }
+            if(option==0)
+            {
+                addressBook.showContacts();
+            }
+
+            System.out.println("Do you want to continue: (yes/no) : ");
+            op=scanner.next();
+        }while(op.equalsIgnoreCase("yes"));
+
     }
 }
