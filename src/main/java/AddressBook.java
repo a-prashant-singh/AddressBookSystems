ec00d7f1b7f9f0956c508f28a50fe6bd3ac3d3d7
parshya <prashsingh1997@gmail.com>
2024-06-10 11:41:08 +0530

    Added Contact class and AddressBook class

---
+++ src/main/java/AddressBook.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+
+
+}
